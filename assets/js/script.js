112c2df5d3189745236c6f7aecf0992f64149964
eduardoVela <eduardovela2022@icloud.com>
2024-04-21 15:41:03 -0600

    Implemented the modal form the site requires to add a new task, the logic necessary to render the cards of the different card lanes, and the logic to drag and drop a card into a different lane.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,206 @@
+// UI elements
+const addTaskButton = $("#add-task-button");
+// Form fields
+const newTaskTitle = $("#task-title");
+const newTaskDueDate = $("#task-due-date");
+const newTaskDescription = $("#task-description");
+// Card lanes
+const toDoCardsLane = $("#todo-cards");
+const inProgressCardsLane = $("#in-progress-cards");
+const doneCardsLane = $("#done-cards");
+// Card lane boxes
+const toDoCardsLaneBox = $("#todo-cards-box");
+const inProgressCardsLaneBox = $("#in-progress-cards-box");
+const doneCardsLaneBox = $("#done-cards-box");
+
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = [];
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
+// Modal form
+modal = $("#task-form").dialog({
+  autoOpen: false,
+  height: 400,
+  width: 350,
+  modal: true,
+  buttons: {
+    "Create task": createTask,
+    Cancel: function () {
+      // Modal form fields are reset
+      resetForm();
+      //  Modal form window is closed
+      modal.dialog("close");
+    },
+  },
+  close: function () {
+    // Modal form fields are reset
+    resetForm();
+  },
+});
 
+function createTask() {
+  // New task is created
+  const newTask = {
+    id: crypto.randomUUID(),
+    title: newTaskTitle.val(),
+    dueDate: newTaskDueDate.val(),
+    description: newTaskDescription.val(),
+    status: "To do",
+  };
+
+  // New task is added to the task list
+  taskList.push(newTask);
+
+  // Task list is saved to local storage
+  saveTasks();
+
+  // Task list is refreshed
+  loadTasks();
+
+  // Card lanes are refreshed
+  renderCardLanes();
+
+  // Modal form fields are reset
+  resetForm();
+
+  //  Modal form window is closed
+  modal.dialog("close");
 }
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
-
+function resetForm() {
+  // Modal form fields are reset
+  newTaskTitle.val("");
+  newTaskDueDate.val("");
+  newTaskDescription.val("");
 }
 
-// Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+function loadTasks() {
+  // Gets task list from local storage
+  taskList = JSON.parse(localStorage.getItem("tasks"));
 
+  // If task list is null, make it an empty array
+  if (taskList === null) {
+    taskList = [];
+  }
 }
 
-// Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(taskList));
 }
 
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function renderCardLanes() {
+  // Card lanes are emptied, so they can be refreshed
+  toDoCardsLane.children().remove("div");
+  inProgressCardsLane.children().remove("div");
+  doneCardsLane.children().remove("div");
 
+  // Gets tasks with the status of "To do"
+  const toDoCards = taskList.filter((task) => task.status === "To do");
+  // Gets tasks with the status of "In progress"
+  const inProgressCards = taskList.filter(
+    (task) => task.status === "In progress"
+  );
+  // Gets tasks with the status of "Done"
+  const doneCards = taskList.filter((task) => task.status === "Done");
+
+  // Renders "To do" cards
+  render(toDoCards, toDoCardsLane);
+
+  // Renders "In progress" cards
+  render(inProgressCards, inProgressCardsLane);
+
+  // Renders "Done" cards
+  render(doneCards, doneCardsLane);
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+// Renders the cards of a card lane
+function render(cardArray, cardLane) {
+  for (const card of cardArray) {
+    // New card draggable is created
+    const newCard = $("<div>").draggable({ opacity: 0.7, helper: "clone" });
+    // It is given the id of the card
+    newCard.attr("id", card.id);
+    // It is given a z index, so that it is not covered by other elements
+    newCard.css("z-index", "1");
+    newCard.css("background-color", "red");
+
+    // Card's title element is created
+    const newCardTitle = $("<h3>");
+    newCardTitle.text(card.title);
+
+    // Card's due date element is created
+    const newCardDueDate = $("<p>");
+    newCardDueDate.text(card.dueDate);
+
+    // Card's description element is created
+    const newCardDescription = $("<p>");
+    newCardDescription.text(card.description);
+
+    // Card's elements are appened to it
+    newCard.append(newCardTitle);
+    newCard.append(newCardDueDate);
+    newCard.append(newCardDescription);
+
+    // Card is appended to the given card lane
+    cardLane.append(newCard);
+  }
+}
+
+function handleAddTask(event) {
+  event.preventDefault();
+
+  // Modal form window is opened
+  modal.dialog("open");
+}
+
 function handleDrop(event, ui) {
+  // Gets the task that triggered the event from the task list
+  const droppedTaskItem = taskList
+    .filter((task) => task.id === ui.draggable[0].id)
+    .at(0);
 
+  // Changes the status of the task, depending on which card lane container it was dropped on
+  switch (event.target.id) {
+    // Dropped on the "To do" card lane
+    case "todo-cards-box":
+      droppedTaskItem.status = "To do";
+      break;
+    // Dropped on the "In progress" card lane
+    case "in-progress-cards-box":
+      droppedTaskItem.status = "In progress";
+      break;
+    // Dropped on the "Done" card lane
+    default:
+      droppedTaskItem.status = "Done";
+      break;
+  }
+
+  // Changes are saved to local storage
+  saveTasks();
+
+  // The updated task list is retrived from local storage
+  loadTasks();
+
+  // Card lanes are refreshed
+  renderCardLanes();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  // Card lane containers are made droppable
+  toDoCardsLaneBox.droppable({
+    drop: handleDrop,
+  });
+  inProgressCardsLaneBox.droppable({
+    drop: handleDrop,
+  });
+  doneCardsLaneBox.droppable({
+    drop: handleDrop,
+  });
 
+  // Tasks are loaded from local storage
+  loadTasks();
+
+  // Card lanes are rendered
+  renderCardLanes();
+  addTaskButton.on("click", handleAddTask);
 });
